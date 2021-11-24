556e21c2f370771c3df833a90358f7b9bfca53ca
EFE ACER <efeacer@gmail.com>
2021-11-24 08:52:20 +0000

    Add files via upload

---
+++ Medium55/MaxSubsetSumNoAdjacent.py
@@ -0,0 +1,17 @@
+def maxSubsetSumNoAdjacent(array):
+	"""
+	Clever DP: O(n)
+	"""
+    if not array:
+		return 0
+	n = len(array)
+	if n == 1:
+		return array[0]
+	dp = [0] * n
+	dp_nadj = array[0]
+	dp_adj = max(dp_nadj, array[1])
+	for i in range(2, n):
+		dp_new = max(dp_adj, array[i] + dp_nadj)
+		dp_nadj = dp_adj
+		dp_adj = dp_new
+	return dp_adj
